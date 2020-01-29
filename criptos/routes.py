25fa7404a34c3a1e765ee83e867c6e0989fe8eff
Pelayo <pelayada7@gmail.com>
2020-01-29 20:31:56 +0100

    Empezar BD

--- criptos/routes.py
+++ criptos/routes.py
@@ -2,11 +2,26 @@ from criptos import app
 from flask import render_template
 import sqlite3
 
+BASE_DATOS = './data/mov.db'
+
+def todosMovDB():
+    conn = sqlite3.connect(BASE_DATOS)
+    cursor = conn.cursor()
+    consulta = 'SELECT id, date, time, from_currency, from_quantity, to_currency, to_quantity FROM Movements'
+    rows = cursor.execute(consulta)
+    filas = []
+    for row in rows:
+        filas.append(row)
+        print(filas)
+    
+    return filas
+
 
 @app.route("/")
 def index():
+    registros = todosMovDB()
 
-    return render_template("index.html")
+    return render_template("index.html", registros=registros)
 
 
 @app.route("/purchase")
